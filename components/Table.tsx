59e232f73a0259daf8cfd5b079dc97407d5bd46c
Jiseeeh <johncarlo.camara1@gmail.com>
2022-11-15 21:49:04 +0800

    feat: programmatically add rows

--- components/Table.tsx
+++ components/Table.tsx
@@ -1,56 +1,21 @@
 import React, { useState, useEffect } from "react";
-import { JsxElement } from "typescript";
 
 import IShorty from "../interfaces/IShorty";
+import syncSessionStorage from "../helper/syncSessionStorage";
+import { IconTrash } from "@tabler/icons";
 
-interface ShortiesTableProps {}
-
-const ShortiesTable: React.FC<ShortiesTableProps> = (props) => {
+const ShortiesTable: React.FC = () => {
   const [shorties, setShorties] = useState<IShorty[]>([]);
-  const [tableContent, setTableContent] = useState<JSX.Element[]>([]);
 
   useEffect(() => {
-    let sessionShorties = sessionStorage.getItem("shorties");
+    const userSessionStorage = sessionStorage.getItem("shorties");
 
-    if (sessionShorties !== null) {
-      setShorties(JSON.parse(sessionShorties));
-
-      setTableContent(
-        shorties.map((data) => (
-          <tr key={data.id}>
-            <th>
-              <label>
-                <input type="checkbox" className="checkbox" />
-              </label>
-            </th>
-            <td>
-              <article className="flex items-center space-x-3">
-                <section className="avatar">
-                  <section className="mask mask-squircle w-12 h-12">
-                    <img
-                      src="https://i.pinimg.com/736x/3a/64/69/3a6469083e633a02f132c2dea8538e8f.jpg"
-                      alt="Anime character sleeping"
-                    />
-                  </section>
-                </section>
-                <article>
-                  <section className="font-bold">Hart Hagerty</section>
-                  <section className="text-sm opacity-50">
-                    Mount Myōboku
-                  </section>
-                </article>
-              </article>
-            </td>
-            <td>{data.key}</td>
-            <td>{data.shorty}</td>
-            <th>
-              <button className="btn btn-ghost btn-xs">details</button>
-            </th>
-          </tr>
-        ))
-      );
+    if (userSessionStorage === "[]") return;
+    if (userSessionStorage !== null) {
+      // only set shorties if it's empty
+      if (shorties.length === 0) setShorties(JSON.parse(userSessionStorage));
     }
-  }, []);
+  }, [shorties]);
 
   return (
     <div className="p-5 overflow-x-auto w-full">
@@ -58,25 +23,62 @@ const ShortiesTable: React.FC<ShortiesTableProps> = (props) => {
         {/* <!-- head --> */}
         <thead>
           <tr>
-            <th>
-              <label>
-                <input type="checkbox" className="checkbox" />
-              </label>
-            </th>
             <th>Name</th>
             <th className="text-center">Long Link</th>
             <th className="text-center">Shorty</th>
             <th>
-              <button className="btn btn-error">Delete</button>
+              <button className="btn btn-error">Delete All</button>
             </th>
           </tr>
         </thead>
-        <tbody>{tableContent}</tbody>
-        {/* <!-- foot --> */}
+        <tbody>
+          {shorties.map((data) => (
+            <tr key={data.id}>
+              <td>
+                <article className="flex items-center space-x-3">
+                  <section className="avatar">
+                    <section className="mask mask-squircle w-12 h-12">
+                      <img
+                        src="https://i.pinimg.com/736x/3a/64/69/3a6469083e633a02f132c2dea8538e8f.jpg"
+                        alt="Anime character sleeping"
+                      />
+                    </section>
+                  </section>
+                  <article>
+                    <section className="font-bold">Hart Hagerty</section>
+                    <section className="text-sm opacity-50">
+                      Mount Myōboku
+                    </section>
+                  </article>
+                </article>
+              </td>
+              <td>{data.key}</td>
+              <td>{data.shorty}</td>
+              <th>
+                <button
+                  className="btn btn-ghost btn-xs"
+                  onClick={() => {
+                    setShorties((prevShorties) =>
+                      prevShorties.filter((shorty) => {
+                        if (shorty.id === data.id) {
+                          syncSessionStorage(shorty, "delete");
+                          return false;
+                        }
+                        return true;
+                      })
+                    );
+                  }}
+                >
+                  <IconTrash />
+                </button>
+              </th>
+            </tr>
+          ))}
+        </tbody>
+        {/* only show footer when there is at-least 1 row */}
         {shorties.length === 0 ? null : (
           <tfoot>
             <tr>
-              <th></th>
               <th>Name</th>
               <th className="text-center">Long Link</th>
               <th className="text-center">Shorty</th>
