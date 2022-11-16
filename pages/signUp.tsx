9b019aa5c578907e77e8d7741d25ce730546821a
Jiseeeh <johncarlo.camara1@gmail.com>
2022-11-16 21:19:27 +0800

    feat: sign up and setting info to session storage

--- pages/signUp.tsx
+++ pages/signUp.tsx
@@ -1,16 +1,34 @@
 import React from "react";
+import axios from "axios";
+import { toast } from "react-hot-toast";
+import { useRouter } from "next/router";
 
 import UserForm from "../components/UserForm";
 
 const SignUp: React.FC = () => {
-  const handleSubmit = (
+  const router = useRouter();
+  const handleSubmit = async (
     e: React.FormEvent<HTMLFormElement>,
     username: string,
     password: string
   ) => {
     e.preventDefault();
-    alert(username);
-    alert(password);
+
+    const userInfo = {
+      name: username,
+      password,
+    };
+
+    const response = await axios.post("/api/sign-up", userInfo);
+
+    if (response.data.success) {
+      toast.success("Sign up success!");
+      router.push("/login");
+      sessionStorage.setItem("userInfo", JSON.stringify(response.data.user));
+      return;
+    }
+
+    toast.error("Username is already taken!");
   };
 
   return (
