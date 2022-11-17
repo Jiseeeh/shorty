21fc03d5d5ea52c200e78e7ff52ced2da7a33ccc
Jiseeeh <johncarlo.camara1@gmail.com>
2022-11-17 14:26:21 +0800

    feat: set user session on login

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,17 +1,37 @@
 import React from "react";
 import Head from "next/head";
+import axios from "axios";
+import toast from "react-hot-toast";
+import { useRouter } from "next/router";
 
 import UserForm from "../components/UserForm";
 
 const Login: React.FC = () => {
-  const handleSubmit = (
+  const router = useRouter();
+  const handleSubmit = async (
     e: React.FormEvent<HTMLFormElement>,
     username: string,
     password: string
   ) => {
     e.preventDefault();
-    alert(username);
-    alert(password);
+
+    const userInfo = {
+      name: username,
+      password,
+    };
+
+    const response = await axios.post("/api/login", userInfo);
+
+    if (response.data.success) {
+      toast.success("Login success!");
+
+      sessionStorage.setItem("userInfo", JSON.stringify(response.data.user));
+
+      router.push("/shorties");
+      return;
+    }
+
+    toast.error("Wrong credentials!");
   };
 
   return (
