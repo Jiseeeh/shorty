e1a16008698ea3b2fa43e6e1ec0f3760549b4d33
Jiseeeh <johncarlo.camara1@gmail.com>
2022-11-18 13:11:23 +0800

    feat: add shorty to user on create

--- pages/api/create.ts
+++ pages/api/create.ts
@@ -6,15 +6,22 @@ export default async function create(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  const { key, value } = req.body;
+  const { key, value, userId } = req.body;
   const shorty = `${process.env.FRONTEND_URL}/${value}`;
+
+  const user = await prisma.user.findFirst({
+    where: {
+      id: userId,
+    },
+  });
 
   const createdShorty = await prisma.shorty.create({
     data: {
+      ownerName: user?.name,
       key,
       value: shorty,
     },
   });
 
-  res.json({ id: createdShorty.id, key, value:shorty });
+  res.json({ id: createdShorty.id, key, value: shorty });
 }
