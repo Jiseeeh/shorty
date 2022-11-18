633422510b0e23e2aee8114b88b93d2e12670041
Jiseeeh <johncarlo.camara1@gmail.com>
2022-11-18 13:11:41 +0800

    feat: api route for getting user shorties

---
+++ pages/api/shorties.ts
@@ -0,0 +1,17 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+import { prisma } from "../../lib/prisma";
+
+export default async function shorties(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const { id } = req.query;
+
+  const userShorties = await prisma.user.findFirst({
+    where: { id: String(id) },
+    select: { shorties: true },
+  });
+
+  res.json(userShorties);
+}
