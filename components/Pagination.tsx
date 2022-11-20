ee23a5160da48c8246b9465f58e858fc9c2ba35b
Jiseeeh <johncarlo.camara1@gmail.com>
2022-11-20 21:54:14 +0800

    feat: add pagination component

---
+++ components/Pagination.tsx
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+
+interface PaginationProps {
+  totalShorties: number;
+  shortyPerPage: number;
+  currentPage: number;
+  setCurrentPage: React.Dispatch<React.SetStateAction<number>>;
+}
+
+const Pagination: React.FC<PaginationProps> = (props) => {
+  const [buttonsPerPage] = useState(5);
+  const [currentButtonPage, setCurrentButtonPage] = useState(1);
+  const lastButtonPageIndex = currentButtonPage * buttonsPerPage;
+  const firstButtonPageIndex = lastButtonPageIndex - buttonsPerPage;
+  const pages: number[] = [];
+
+  for (
+    let i = 1;
+    i <= Math.ceil(props.totalShorties / props.shortyPerPage);
+    i++
+  )
+    pages.push(i);
+
+  const maxButtonPage = pages.length / buttonsPerPage;
+  const minButtonPage = 0;
+
+  return (
+    <section className="btn-group">
+      {pages.length > buttonsPerPage && (
+        <button
+          className="btn"
+          onClick={() => {
+            if (currentButtonPage - 1 <= minButtonPage) return;
+            setCurrentButtonPage((prevPage) => prevPage - 1);
+          }}
+        >
+          «
+        </button>
+      )}
+      {pages
+        .slice(firstButtonPageIndex, lastButtonPageIndex)
+        .map((pageNumber) => (
+          <button
+            key={pageNumber}
+            className={`btn ${
+              props.currentPage === pageNumber ? "btn-primary" : ""
+            }`}
+            onClick={() => {
+              props.setCurrentPage(pageNumber);
+            }}
+          >
+            {pageNumber}
+          </button>
+        ))}
+      {pages.length > buttonsPerPage && (
+        <button
+          className="btn"
+          onClick={() => {
+            console.log(currentButtonPage + 1);
+            console.log(maxButtonPage);
+            if (currentButtonPage + 1 > maxButtonPage) return;
+            setCurrentButtonPage((prevPage) => prevPage + 1);
+          }}
+        >
+          »
+        </button>
+      )}
+    </section>
+  );
+};
+
+export default Pagination;
