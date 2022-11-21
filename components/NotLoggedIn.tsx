151614bdad2d733aa5c32f9276723f80918136cc
Jiseeeh <johncarlo.camara1@gmail.com>
2022-11-21 15:39:52 +0800

    style: increase h1 font size

--- components/NotLoggedIn.tsx
+++ components/NotLoggedIn.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 const NotLoggedIn: React.FC = () => {
   return (
     <section className="flex flex-col gap-3 text-center">
-      <h1>❗You are not logged in❗</h1>
+      <h1 className="font-bold text-2xl">❗You are not logged in❗</h1>
       <Link className="link link-accent" href="/signUp">
         Sign up
       </Link>
