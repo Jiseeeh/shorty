1d29dbc60a2ad9f2acf7f0a22bc707dc19683782
Jiseeeh <johncarlo.camara1@gmail.com>
2022-11-23 21:22:43 +0800

    feat: replace heading content

--- components/NotLoggedIn.tsx
+++ components/NotLoggedIn.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 const NotLoggedIn: React.FC = () => {
   return (
     <section className="flex flex-col gap-3 text-center">
-      <h1 className="font-bold text-2xl">❗You are not logged in❗</h1>
+      <h1 className="font-bold text-2xl">❗Please sign up or login first❗</h1>
       <Link className="link link-accent" href="/signUp">
         Sign up
       </Link>
