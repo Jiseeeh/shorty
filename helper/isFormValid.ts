a3e333225b890cf98442aeb1385e199f3b50eb09
Jiseeeh <johncarlo.camara1@gmail.com>
2022-11-27 22:34:29 +0800

    feat: add validation for step 2

--- helper/isFormValid.ts
+++ helper/isFormValid.ts
@@ -17,10 +17,17 @@ export default function isFormValid(data: IResetForm) {
       }
 
       isValid = true;
+      break;
 
-      break;
     case 2:
+      if (data.answer.length > 12) {
+        message = "Domain too long!";
+        break;
+      }
+
+      isValid = true;
       break;
+
     case 3:
       if (data.newPassword.length === 0 || data.confirmPassword.length === 0) {
         message = "Please fill out all the fields.";
